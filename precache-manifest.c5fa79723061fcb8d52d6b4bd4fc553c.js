ebc4df49b0d08cb02e273281e941242410bfc15c
k-mayur <mayur.kumar@mountblue.io>
2019-04-24 12:49:12 +0530

    Updates

---
+++ precache-manifest.c5fa79723061fcb8d52d6b4bd4fc553c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "999be55473b9c95136fb880f778cc9c3",
+    "url": "/github-issues/static/media/err.999be554.png"
+  },
+  {
+    "revision": "950fa1b6e3bbf97c5a41",
+    "url": "/github-issues/static/js/runtime~main.02425164.js"
+  },
+  {
+    "revision": "0c185d7f9e75358d97c6",
+    "url": "/github-issues/static/js/main.45b28408.chunk.js"
+  },
+  {
+    "revision": "e5f9dc9135b56e7cf905",
+    "url": "/github-issues/static/js/2.39dca2f4.chunk.js"
+  },
+  {
+    "revision": "0c185d7f9e75358d97c6",
+    "url": "/github-issues/static/css/main.83179e62.chunk.css"
+  },
+  {
+    "revision": "e5f9dc9135b56e7cf905",
+    "url": "/github-issues/static/css/2.352963b6.chunk.css"
+  },
+  {
+    "revision": "121b0ac9956934ea4a848d23f8ba6bb9",
+    "url": "/github-issues/index.html"
+  }
+];
